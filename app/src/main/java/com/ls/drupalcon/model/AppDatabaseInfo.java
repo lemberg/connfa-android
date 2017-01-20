88ecf2185319eae50ff78e66247ab5e099e194ac
Taras Kunyk <taras.kunyk@lemberg.co.uk>
2017-01-20 14:02:04 +0200

    1. Crash on migration fixed

--- app/src/main/java/com/ls/drupalcon/model/AppDatabaseInfo.java
+++ app/src/main/java/com/ls/drupalcon/model/AppDatabaseInfo.java
@@ -3,6 +3,7 @@ package com.ls.drupalcon.model;
 import com.ls.drupalcon.R;
 import com.ls.drupalcon.app.App;
 import com.ls.drupalcon.model.dao.EventDao;
+import com.ls.drupalcon.model.dao.FloorPlanDao;
 import com.ls.drupalcon.model.dao.InfoDao;
 import com.ls.drupalcon.model.dao.LevelDao;
 import com.ls.drupalcon.model.dao.LocationDao;
@@ -55,6 +56,7 @@ public class AppDatabaseInfo implements DBInfo, IMigrationTask {
         dbTableNameList.add(InfoDao.TABLE_NAME);
         dbTableNameList.add(TABLE_EVENT_SPEAKER);
         dbTableNameList.add(TABLE_FAVORITE_EVENTS);
+        dbTableNameList.add(FloorPlanDao.TABLE_NAME);
         return dbTableNameList;
     }
 
