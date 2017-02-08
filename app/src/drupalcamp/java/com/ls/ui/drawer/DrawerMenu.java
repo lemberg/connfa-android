1e4b52bda2cef2a34829005334f10088b8b5243d
Pavlo Savchuk <pavlo.savchuk@lemberg.co.uk>
2017-02-08 15:39:47 +0200

    drupalcamp product Flavors was customized

--- app/src/drupalcamp/java/com/ls/ui/drawer/DrawerMenu.java
+++ app/src/drupalcamp/java/com/ls/ui/drawer/DrawerMenu.java
@@ -8,8 +8,11 @@ import com.ls.drupalcon.app.App;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DrawerMenu {
+/**
+ * Created by And on 2/8/2017.
+ */
 
+public class DrawerMenu {
     public enum EventMode {Program, Bofs, Social, SocialMedia, Favorites, FloorPlan, Location, Speakers, About}
 
     private static final int[] MENU_ICON_RES = {
@@ -52,15 +55,15 @@ public class DrawerMenu {
     public static List<DrawerMenuItem> getNavigationDrawerItems() {
         Context context = App.getContext();
         List<DrawerMenuItem> result = new ArrayList<>();
-        result.add(new DrawerMenuItem(context.getString(R.string.Sessions), R.drawable.menu_icon_program, R.drawable.menu_icon_program_sel, EventMode.Program, false));
-        result.add(new DrawerMenuItem(context.getString(R.string.bofs), R.drawable.menu_icon_bofs, R.drawable.menu_icon_bofs_sel, EventMode.Bofs, false));
-        result.add(new DrawerMenuItem(context.getString(R.string.social_events), R.drawable.menu_icon_social, R.drawable.menu_icon_social_sel, EventMode.Social, false));
-        result.add(new DrawerMenuItem(context.getString(R.string.social_media), R.drawable.menu_icon_social_media, R.drawable.menu_icon_social_media_sel, EventMode.SocialMedia, true));
-        result.add(new DrawerMenuItem(context.getString(R.string.my_schedule), R.drawable.menu_icon_my_schedule, R.drawable.menu_icon_my_schedule_sel, EventMode.Favorites, false));
-        result.add(new DrawerMenuItem(context.getString(R.string.floor_plan), R.drawable.menu_icon_floor_plan, R.drawable.menu_icon_floor_plan_sel, EventMode.FloorPlan, false));
-        result.add(new DrawerMenuItem(context.getString(R.string.location), R.drawable.menu_icon_location, R.drawable.menu_icon_location_sel, EventMode.Location, false));
-        result.add(new DrawerMenuItem(context.getString(R.string.speakers), R.drawable.menu_icon_speakers, R.drawable.menu_icon_speakers_sel, EventMode.Speakers, true));
-        result.add(new DrawerMenuItem(context.getString(R.string.about), R.drawable.menu_icon_about, R.drawable.menu_icon_about_sel, EventMode.About, false));
+        result.add(new DrawerMenuItem(context.getString(R.string.Sessions), R.drawable.menu_icon_program, R.drawable.menu_icon_program_sel, DrawerMenu.EventMode.Program, false));
+//        result.add(new DrawerMenuItem(context.getString(R.string.bofs), R.drawable.menu_icon_bofs, R.drawable.menu_icon_bofs_sel, EventMode.Bofs, false));
+//        result.add(new DrawerMenuItem(context.getString(R.string.social_events), R.drawable.menu_icon_social, R.drawable.menu_icon_social_sel, EventMode.Social, false));
+        result.add(new DrawerMenuItem(context.getString(R.string.social_media), R.drawable.menu_icon_social_media, R.drawable.menu_icon_social_media_sel, DrawerMenu.EventMode.SocialMedia, true));
+        result.add(new DrawerMenuItem(context.getString(R.string.my_schedule), R.drawable.menu_icon_my_schedule, R.drawable.menu_icon_my_schedule_sel, DrawerMenu.EventMode.Favorites, false));
+        result.add(new DrawerMenuItem(context.getString(R.string.floor_plan), R.drawable.menu_icon_floor_plan, R.drawable.menu_icon_floor_plan_sel, DrawerMenu.EventMode.FloorPlan, false));
+        result.add(new DrawerMenuItem(context.getString(R.string.location), R.drawable.menu_icon_location, R.drawable.menu_icon_location_sel, DrawerMenu.EventMode.Location, false));
+        result.add(new DrawerMenuItem(context.getString(R.string.speakers), R.drawable.menu_icon_speakers, R.drawable.menu_icon_speakers_sel, DrawerMenu.EventMode.Speakers, true));
+        result.add(new DrawerMenuItem(context.getString(R.string.about), R.drawable.menu_icon_about, R.drawable.menu_icon_about_sel, DrawerMenu.EventMode.About, false));
 
         return result;
     }
