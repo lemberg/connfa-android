1be620a7d7c4cfa69d209797248004fb7e844116
Pavlo Savchuk <pavlo.savchuk@lemberg.co.uk>
2017-02-08 13:53:43 +0200

    two flower for con and camp were implemented

---
+++ app/src/drupalconapp/java/com/ls/ui/drawer/DrawerMenuItem.java
@@ -0,0 +1,53 @@
+package com.ls.ui.drawer;
+
+
+public class DrawerMenuItem {
+    private String name;
+    private int iconRes;
+    private int selIconRes;
+    private boolean group;
+    private DrawerMenu.EventMode eventMode;
+
+    public DrawerMenuItem(String name, int iconRes, int selIconRes, DrawerMenu.EventMode eventMode, boolean group) {
+        this.name = name;
+        this.iconRes = iconRes;
+        this.selIconRes = selIconRes;
+        this.eventMode = eventMode;
+        this.group = group;
+    }
+
+
+    public int getIconRes() {
+        return iconRes;
+    }
+
+
+    public int getSelIconRes() {
+        return selIconRes;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public boolean isGroup() {
+        return group;
+    }
+
+    public DrawerMenu.EventMode getEventMode() {
+        return eventMode;
+    }
+
+    @Override
+    public String toString() {
+        return "DrawerMenuItem{" +
+                "name='" + name + '\'' +
+                ", iconRes=" + iconRes +
+                ", selIconRes=" + selIconRes +
+                ", eventMode=" + eventMode +
+                ", group=" + group +
+                '}';
+    }
+}
