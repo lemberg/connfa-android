66307fcbaf0dc906a5837876ab3539ee153d7b8f
Pavlo Savchuk <pavlo.savchuk@lemberg.co.uk>
2017-04-26 11:08:01 +0300

    update request fix; UI hotfix. touch[1662]

--- app/src/main/java/com/ls/drupalcon/model/UpdateRequest.java
+++ app/src/main/java/com/ls/drupalcon/model/UpdateRequest.java
@@ -26,7 +26,9 @@ public enum UpdateRequest {
     @SerializedName("10")
     POIS(10),
     @SerializedName("11")
-    INFO(11);
+    INFO(11),
+    @SerializedName("12")
+    SCHEDULE(12);
 
     private int requestId;
 
