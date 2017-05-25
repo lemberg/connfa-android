9b5d1a79e1f30011709c4d578c927fdcf2d1c79f
Pavlo Savchuk <pavlo.savchuk@lemberg.co.uk>
2017-05-25 11:02:11 +0300

    CustomScrollView

---
+++ app/src/main/java/com/ls/ui/view/CustomScrollView.java
@@ -0,0 +1,49 @@
+package com.ls.ui.view;
+
+import android.content.Context;
+import android.support.v4.widget.NestedScrollView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class CustomScrollView extends NestedScrollView {
+
+    private boolean enableScrolling = false;
+
+    public boolean isEnableScrolling() {
+        return enableScrolling;
+    }
+
+    public void setEnableScrolling(boolean enableScrolling) {
+        this.enableScrolling = enableScrolling;
+    }
+
+    public CustomScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CustomScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomScrollView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        if (isEnableScrolling()) {
+            return super.onInterceptTouchEvent(ev);
+        } else {
+            return false;
+        }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (isEnableScrolling()) {
+            return super.onTouchEvent(ev);
+        } else {
+            return false;
+        }
+    }
+}
